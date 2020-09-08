9199964036ced6b5a351de590f90d8a658f02b19
Simon Young <simon.young83@me.com>
2020-09-08 20:57:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 function happyHolidays (string) {return "Happy holidays!"}
 function happyHolidaysTo (Emma) {return (`Happy holidays, ${Emma}!`) }
+function happyCustomHolidayTo (holiday, name) {return `Happy ${holiday}, ${name}!`}
