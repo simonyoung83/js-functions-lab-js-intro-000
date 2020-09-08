9c630d156118ca4ed5e6df7c7d74b5e84229868d
Simon Young <simon.young83@me.com>
2020-09-08 21:01:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 function happyHolidays (string) {return "Happy holidays!"}
 function happyHolidaysTo (Emma) {return (`Happy holidays, ${Emma}!`) }
 function happyCustomHolidayTo (holiday, name) {return `Happy ${holiday}, ${name}!`}
+function holidayCountdown (days, holiday) {return `It's ${days}days until ${holiday}!`}
